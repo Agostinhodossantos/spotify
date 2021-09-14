f9719149f39c9711ad91ab56d6159a195fddcabe
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-09-14 20:48:10 +0200

    add new dependency

--- lib/pages/root_app.dart
+++ lib/pages/root_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spotify/theme/colors.dart';
 
 class RootApp extends StatefulWidget {
   
@@ -9,10 +10,48 @@ class RootApp extends StatefulWidget {
 }
 
 class _RootAppState extends State<RootApp> {
+  int activeTab = 0;
+   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: ,
+      backgroundColor: black,
+      bottomNavigationBar: getFooter(),
     );
   }
+
+  Widget getFooter() {
+
+  List items = [
+      Feather.home,
+      Feather.book,
+      Feather.search,
+      Feather.settings,
+  ];
+
+  return Container(
+    height: 80,
+    decoration: BoxDecoration(color: black),
+    child: Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: List.generate(4, (index) {
+          return IconButton(
+          onPressed: () {
+           setState(() {
+             activeTab = index;
+           });
+          }, 
+          icon: Icon(
+            Icons.home, 
+            color: white,
+          ));
+        })
+      ),
+    ),
+  );
 }
+}
+
